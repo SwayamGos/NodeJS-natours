4c21976b9c1456139aacaaf7e43cb7d9e656a5af
SwayamGos <swayamgoswami989@gmail.com>
2023-07-21 01:31:54 +0530

    Built js files

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,5 +1,5 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */ // import '@babel/polyfill';
-/* eslint-disable */ const $96761fcf66c88173$export$4c5dd147b21b9176 = (locations)=>{
+/* eslint-disable */ const $2ddad2e7221ab550$export$4c5dd147b21b9176 = (locations)=>{
     let map = L.map("map", {
         zoomControl: false
     });
@@ -25,22 +25,22 @@
 };
 
 
-/* eslint-disable */ /* eslint-disable */ const $c6a88cbb7d89dfbc$export$516836c6a9dfc573 = ()=>{
+/* eslint-disable */ /* eslint-disable */ const $043c1b05d48764e0$export$516836c6a9dfc573 = ()=>{
     const el = document.querySelector(".alert");
     if (el) {
         console.log(el.parentElement, el);
         el.parentElement.removeChild("el");
     }
 };
-const $c6a88cbb7d89dfbc$export$de026b00723010c1 = (type, msg)=>{
-    $c6a88cbb7d89dfbc$export$516836c6a9dfc573();
+const $043c1b05d48764e0$export$de026b00723010c1 = (type, msg)=>{
+    $043c1b05d48764e0$export$516836c6a9dfc573();
     const markup = `<div class="alert alert--${type}">${msg}</div>`;
     document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
-    window.setTimeout($c6a88cbb7d89dfbc$export$516836c6a9dfc573, 5000);
+    window.setTimeout($043c1b05d48764e0$export$516836c6a9dfc573, 5000);
 };
 
 
-const $2182bd3af7eff4ff$export$596d806903d1f59e = async (obj)=>{
+const $ca410237411227bc$export$596d806903d1f59e = async (obj)=>{
     try {
         let res = await fetch("/api/v1/users/login", {
             method: "POST",
@@ -56,35 +56,35 @@ const $2182bd3af7eff4ff$export$596d806903d1f59e = async (obj)=>{
         res = await res.json();
         console.log(res);
         if (res.status === "success") {
-            (0, $c6a88cbb7d89dfbc$export$de026b00723010c1)("success", "Logged in successfully");
+            (0, $043c1b05d48764e0$export$de026b00723010c1)("success", "Logged in successfully");
             window.setTimeout(()=>{
                 location.assign("/");
             }, 1000);
         } else throw err;
     } catch (err1) {
         // console.log('hi');
-        (0, $c6a88cbb7d89dfbc$export$de026b00723010c1)("error", "Please provide valid email or password");
+        (0, $043c1b05d48764e0$export$de026b00723010c1)("error", "Please provide valid email or password");
     }
 };
-const $2182bd3af7eff4ff$export$a0973bcfe11b05c9 = async ()=>{
+const $ca410237411227bc$export$a0973bcfe11b05c9 = async ()=>{
     try {
         let res = await fetch("/api/v1/users/logout");
         console.log(res);
         if (res.status === 200) location.reload(true);
     } catch (err1) {
         // console.log(err);
-        (0, $c6a88cbb7d89dfbc$export$de026b00723010c1)("error", "Error Logging out.");
+        (0, $043c1b05d48764e0$export$de026b00723010c1)("error", "Error Logging out.");
     }
 };
 
 
 /*eslint-disable*/ 
-const $fc2892257a9a50b3$var$jsonFormData = async (formData)=>{
+const $ad48562da4fc0496$var$jsonFormData = async (formData)=>{
     const plainFormData = Object.fromEntries(formData.entries());
     // console.log(plainFormData);
     return JSON.stringify(plainFormData);
 };
-const $fc2892257a9a50b3$export$f558026a994b6051 = async (data, type)=>{
+const $ad48562da4fc0496$export$f558026a994b6051 = async (data, type)=>{
     const dt = {
         ...data
     };
@@ -107,12 +107,12 @@ const $fc2892257a9a50b3$export$f558026a994b6051 = async (data, type)=>{
             form.append("email", dt.email);
             form.append("photo", dt.photo[0]);
             // console.log(dt.photo[0]);
-            options.body = await $fc2892257a9a50b3$var$jsonFormData(form);
+            options.body = await $ad48562da4fc0496$var$jsonFormData(form);
         // console.log(await options.body);
         }
         let res = await fetch(url, options);
         if (res.status === 200) // console.log('hi');
-        (0, $c6a88cbb7d89dfbc$export$de026b00723010c1)("success", `${type.toUpperCase()} updated successfully`);
+        (0, $043c1b05d48764e0$export$de026b00723010c1)("success", `${type.toUpperCase()} updated successfully`);
     } catch (err) {
         console.log("error", err);
     }
@@ -120,44 +120,44 @@ const $fc2892257a9a50b3$export$f558026a994b6051 = async (data, type)=>{
 
 
 // DOM ELEMENTS
-const $691131ff80032412$var$leaflet = document.getElementById("map");
-const $691131ff80032412$var$loginForm = document.querySelector(".form--login");
-const $691131ff80032412$var$logoutBtn = document.querySelector(".nav__el--logout");
-const $691131ff80032412$var$userDataForm = document.querySelector(".form-user-data");
-const $691131ff80032412$var$userPasswordForm = document.querySelector(".form-user-password");
+const $c7b63642207e38e0$var$leaflet = document.getElementById("map");
+const $c7b63642207e38e0$var$loginForm = document.querySelector(".form--login");
+const $c7b63642207e38e0$var$logoutBtn = document.querySelector(".nav__el--logout");
+const $c7b63642207e38e0$var$userDataForm = document.querySelector(".form-user-data");
+const $c7b63642207e38e0$var$userPasswordForm = document.querySelector(".form-user-password");
 // VALUES
-if ($691131ff80032412$var$leaflet) {
+if ($c7b63642207e38e0$var$leaflet) {
     const locations = JSON.parse(document.getElementById("map").dataset.locations);
-    (0, $96761fcf66c88173$export$4c5dd147b21b9176)(locations);
+    (0, $2ddad2e7221ab550$export$4c5dd147b21b9176)(locations);
 // console.log(locations);
 }
-if ($691131ff80032412$var$loginForm) $691131ff80032412$var$loginForm.addEventListener("submit", (e)=>{
+if ($c7b63642207e38e0$var$loginForm) $c7b63642207e38e0$var$loginForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    (0, $2182bd3af7eff4ff$export$596d806903d1f59e)({
+    (0, $ca410237411227bc$export$596d806903d1f59e)({
         email: email,
         password: password
     });
 });
-if ($691131ff80032412$var$logoutBtn) $691131ff80032412$var$logoutBtn.addEventListener("click", (0, $2182bd3af7eff4ff$export$a0973bcfe11b05c9));
-if ($691131ff80032412$var$userDataForm) $691131ff80032412$var$userDataForm.addEventListener("submit", (e)=>{
+if ($c7b63642207e38e0$var$logoutBtn) $c7b63642207e38e0$var$logoutBtn.addEventListener("click", (0, $ca410237411227bc$export$a0973bcfe11b05c9));
+if ($c7b63642207e38e0$var$userDataForm) $c7b63642207e38e0$var$userDataForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const photo = document.getElementById("photo").files;
-    (0, $fc2892257a9a50b3$export$f558026a994b6051)({
+    (0, $ad48562da4fc0496$export$f558026a994b6051)({
         name: name,
         email: email,
         photo: photo
     }, "data");
 });
-if ($691131ff80032412$var$userPasswordForm) $691131ff80032412$var$userPasswordForm.addEventListener("submit", async (e)=>{
+if ($c7b63642207e38e0$var$userPasswordForm) $c7b63642207e38e0$var$userPasswordForm.addEventListener("submit", async (e)=>{
     e.preventDefault();
     const passwordCurrent = document.getElementById("password-current").value;
     const password = document.getElementById("password").value;
     const passwordConfirm = document.getElementById("password-confirm").value;
-    await (0, $fc2892257a9a50b3$export$f558026a994b6051)({
+    await (0, $ad48562da4fc0496$export$f558026a994b6051)({
         password: password,
         passwordCurrent: passwordCurrent,
         passwordConfirm: passwordConfirm
