9034e752dbeee2d77d7393656935737b979f1f01
Swayam Goswami <59536611+SwayamGos@users.noreply.github.com>
2023-07-23 18:19:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-// const cors = require('cors');
+const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
@@ -22,8 +22,8 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// app.use(cors());
-// app.options('*', cors());
+app.use(cors());
+app.options('*', cors());
 
 // MIDDLEWARES
 
